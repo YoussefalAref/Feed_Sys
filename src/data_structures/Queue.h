606b5ccc01ab6f3ea0d5a48b8cec758778faa116
janayasserfawzi <janayasserfawzi@github.com>
2026-03-27 20:54:02 +0200

    Queue and Interacton class done

--- src/data_structures/Queue.h
+++ src/data_structures/Queue.h
@@ -1,6 +1,28 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+#include "../models/Interaction.h"
+class Queue {
+private:
+    struct Node {
+        Interaction data;
+        Node* next;
+        Node(Interaction interaction) : data(interaction), next(nullptr) {}
+    };
+
+    Node* front;
+    Node* back;
+    int size;
+public:
+    Queue();
+    ~Queue();
+    void enqueue(Interaction interaction);
+    Interaction dequeue();
+    Interaction peek();
+    bool isEmpty();
+    int getsize();
+    void display();
+};
 /*
  * ============================================================
  *  FILE: src/data_structures/Queue.h
